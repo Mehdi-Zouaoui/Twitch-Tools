43764e0349263d1cd517e97637032c3d3f428f67
Your Name <md1.zouaoui@gmail.com>
2021-10-15 23:14:40 +0200

    connection to twitch done

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,11 +1,20 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPlus } from "@fortawesome/free-solid-svg-icons";
+import Link from "next/link";
+
 const Card = ({ title , color}) => {
   return (
     <div className="toolCard" >
         <div className="topCard">
         <h2>{title}</h2>
-        <button className ="addTool"> <FontAwesomeIcon icon={faPlus} /></button>
+        <button className ="addTool"> 
+        <Link href="/tools/sondage">
+            <a>
+            <FontAwesomeIcon icon={faPlus} />
+            </a>
+          </Link>
+          </button>
+       
         </div>
         <div className="bottomCard">
             <p>Aucun créé pour le moment</p>
--- src/pages/userTools.js
+++ src/pages/userTools.js
@@ -1,19 +1,72 @@
-import { useState , useEffect } from "react";
+import { useState, useEffect } from "react";
 import { useUser } from "@auth0/nextjs-auth0";
+import { ApiClient } from "twitch";
+import axios from "axios";
+const tmi = require("tmi.js");
 
 const userTools = ({ sondages }) => {
-  const { user } = useUser();
-  const [userSondages, setUserSondages] = useState([]);
+  const { user, error, isLoading } = useUser();
+  const [connected, setConnected] = useState(false);
 
-//   useEffect(() => {
-//     sondages.forEach(item =>{
-//         if (user.sub === item.author) {
-//            setUserSondages(old => [...old , item])
-//           }
-//     })
-//   },[])
-  console.log("sondages", user);
-  return <div>userTools</div>;
+  const client = new tmi.Client({
+    options: { debug: true },
+
+    connection: {
+      reconnect: false,
+      secure: true,
+    },
+    channels: ["zerator"],
+  });
+
+  client.on("connected", () => {
+    console.log("The bot is Online!");
+  });
+
+  client.on("disconnected", () => {
+    console.log("The bot is Online!");
+  });
+
+  client.on("message", (channel, tags, message, self) => {
+    // "Alca: Hello, World!"
+    console.log(client.readyState());
+    console.log(`${tags["display-name"]}: ${message}`);
+  });
+
+  const handleTwitchConnect = async () => {
+    console.log(client);
+    if (connected) {
+      await client.disconnect();
+      setConnected(false);
+
+      console.log(client.readyState());
+    } else {
+      await client.connect();
+      setConnected(true);
+      console.log(client.readyState());
+    }
+  };
+
+  if (isLoading) return <div>Loading...</div>;
+  return (
+    <div className="ICI">
+      <div>Bonjour</div>
+      {sondages.map((item, index) => {
+        if (user.sub === item.author) {
+          return (
+            <div key={index}>
+              <div>{item.title}</div>
+              {item.fields.map((field, index) => {
+                return <div key={index}> {field.name} </div>;
+              })}
+              <button onClick={() => handleTwitchConnect()}>
+                {connected ? "STOP" : "START"}
+              </button>
+            </div>
+          );
+        }
+      })}
+    </div>
+  );
 };
 
 export default userTools;
