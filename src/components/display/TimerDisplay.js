db45fa3c9ab8d7f5831ffaa6ee3e2968885cbe7b
Your Name <md1.zouaoui@gmail.com>
2021-12-04 15:26:42 +0100

    wip

--- src/components/display/TimerDisplay.js
+++ src/components/display/TimerDisplay.js
@@ -7,6 +7,7 @@ import {
   faEye,
   faEyeSlash,
   faTrash,
+  faStopwatch
 } from "@fortawesome/free-solid-svg-icons";
 import { CountdownCircleTimer } from "react-countdown-circle-timer";
 import { useRouter } from "next/router";
@@ -19,8 +20,10 @@ const renderTime = ({ remainingTime }) => {
 };
 
 const TimerDisplay = ({ data, timers, setTimers }) => {
+  const interval = useRef();
   const router = useRouter();
   const [started, setStarted] = useState(false);
+  const [redo, setRedo] = useState(false);
   const [time, setTime] = useState(0);
   const [key, setKey] = useState(0);
   const [stream, setStream] = useState(false);
@@ -43,39 +46,43 @@ const TimerDisplay = ({ data, timers, setTimers }) => {
     setTimers(
       timers.map((item) => {
         if (item._id === data._id) {
-          return { ...item, started: true, value: 0 };
+          return { ...item, started: true, value: 0, restart: false };
         }
       })
     );
-
-    // interval.current = setInterval(() => {
-    //   setTime((prevTime) => prevTime + 10);
-    //   console.log("time", ("0" + ((time / 10) % 100)).slice(-2));
-    //   setTimers(
-    //     timers.map((item) =>
-    //       item._id === data._id
-    //         ? {
-    //             ...item,
-    //             started: true,
-    //             value: ("0" + (Math.floor(time / 1000) % 60)).slice(-2),
-    //           }
-    //         : { ...item }
-    //     )
-    //   );
-    // }, 10);
+    clearInterval(interval.current);
+    interval.current = setInterval(() => {
+      setTime((prevTime) => prevTime + 10);
+      console.log("time", ("0" + ((time / 10) % 100)).slice(-2));
+    }, 10);
     console.log("running");
     setStarted(true);
+    setRedo(true);
   };
   const stop = () => {
+    clearInterval(interval.current);
     setTimers(
       timers.map((item) =>
-        item._id === data._id ? { ...item, started: false } : { ...item }
+        item._id === data._id
+          ? { ...item, started: false, restart: false }
+          : { ...item }
       )
     );
 
     setStarted(false);
   };
 
+  const restart = () => {
+    setTime((prevTime) => prevTime * 0);
+    setRedo(false);
+    setTimers(
+      timers.map((item) =>
+        item._id === data._id
+          ? { ...item, restart: true, started: false }
+          : { ...item }
+      )
+    );
+  };
   const removeTimer = (id) => {
     fetch("http://localhost:3000/api/timer/" + id, {
       method: "DELETE",
@@ -88,7 +95,7 @@ const TimerDisplay = ({ data, timers, setTimers }) => {
 
   return (
     <div className="timerContainer">
-      <button
+      {/* <button
         className="timerStream"
         onClick={() => {
           console.log("before", stream);
@@ -107,15 +114,35 @@ const TimerDisplay = ({ data, timers, setTimers }) => {
       </button>
       <button className="counterDelete" onClick={() => removeTimer(data._id)}>
         <FontAwesomeIcon icon={faTrash} />{" "}
-      </button>
-      {data.display === "dial" && (
-        <div className="dial">
+      </button> */}
+      <div className="buttonFormatContainer">
+        <button
+          className="displayChoiceButton defaultChoice"
+          onClick={(e) => {
+            e.preventDefault();
+            // setDisplay("stopwatch");
+          }}
+        >
+          <FontAwesomeIcon icon={faStopwatch} />
+        </button>
+        <button
+          className="displayChoiceButton"
+          onClick={(e) => {
+            e.preventDefault();
+            // setDisplay("dial");
+          }}
+        >
+          <div>00:00</div>
+        </button>
+      </div>
+      <div className="displayTimerContainer">
+      <div className="dial">
           <h3>{data.title}</h3>
           <div className="dialContainer">
             {/* <p>{("0" + (time/10)%100).slice(-2)}</p> */}
-            <p>{("0" + (Math.floor(time / 60000) % 60)).slice(-2)} : </p>
+            {/* <p>{("0" + (Math.floor(time / 60000) % 60)).slice(-2)} : </p>
             <p>{("0" + (Math.floor(time / 1000) % 60)).slice(-2)} : </p>
-            <p>{("0" + ((time / 10) % 100)).slice(-2)}</p>
+            <p>{("0" + ((time / 10) % 100)).slice(-2)}</p> */}
           </div>
           <div className="dialButtonContainer">
             {!started && time === 0 && (
@@ -133,13 +160,47 @@ const TimerDisplay = ({ data, timers, setTimers }) => {
                 <FontAwesomeIcon icon={faPlay} />
               </button>
             )}
-            {!started && time > 0 && (
-              <button onClick={() => setTime((prevTime) => prevTime + 1)}>
+            {!started && redo > 0 && (
+              <button onClick={() => restart()}>
                 <FontAwesomeIcon icon={faRedo} />
               </button>
             )}
           </div>
         </div>
+      </div>
+      {data.display === "dial" && (
+     <div></div>
+        // <div className="dial">
+        //   <h3>{data.title}</h3>
+        //   <div className="dialContainer">
+        //     {/* <p>{("0" + (time/10)%100).slice(-2)}</p> */}
+        //     {/* <p>{("0" + (Math.floor(time / 60000) % 60)).slice(-2)} : </p>
+        //     <p>{("0" + (Math.floor(time / 1000) % 60)).slice(-2)} : </p>
+        //     <p>{("0" + ((time / 10) % 100)).slice(-2)}</p> */}
+        //   </div>
+        //   <div className="dialButtonContainer">
+        //     {!started && time === 0 && (
+        //       <button onClick={() => start()}>
+        //         <FontAwesomeIcon icon={faPlay} />
+        //       </button>
+        //     )}
+        //     {started && (
+        //       <button onClick={() => stop()}>
+        //         <FontAwesomeIcon icon={faPause} />
+        //       </button>
+        //     )}
+        //     {!started && time !== 0 && (
+        //       <button onClick={() => start()}>
+        //         <FontAwesomeIcon icon={faPlay} />
+        //       </button>
+        //     )}
+        //     {!started && redo > 0 && (
+        //       <button onClick={() => restart()}>
+        //         <FontAwesomeIcon icon={faRedo} />
+        //       </button>
+        //     )}
+        //   </div>
+        // </div>
       )}
       {data.display === "stopwatch" && (
         <div className="stopWatchContainer">
--- src/pages/stream/components/StreamedTimer.js
+++ src/pages/stream/components/StreamedTimer.js
@@ -1,26 +1,44 @@
 import { useState, useEffect, useRef } from "react";
 import { CountdownCircleTimer } from "react-countdown-circle-timer";
+const renderTime = ({ remainingTime }) => {
+  return (
+    <div className="timer">
+      <div className="value">{remainingTime}</div>
+    </div>
+  );
+};
+
 const StreamedTimer = ({ data }) => {
   const [index, setIndex] = useState(0);
   const interval = useRef();
   const [time, setTime] = useState(0);
   console.log(data);
+
   if (data.started) {
-    console.log('running')
+    console.log("running");
     clearInterval(interval.current);
     interval.current = setInterval(() => {
-      console.log("started");
-      setTime(time+1)
-    }, 1000);
+      setTime((prevTime) => prevTime + 10);
+      console.log("time", ("0" + ((time / 10) % 100)).slice(-2));
+    }, 10);
   }
-  if(data.started === false) {
+  if (data.started === false) {
     clearInterval(interval.current);
+    if (data.restart === true) {
+      console.log("stoped , you wanna restart ?");
+      setTime((prevTime) => prevTime * 0);
+      data.restart = false;
+    }
   }
 
   return (
     <div className="stream">
       <div>{data.title}</div>
-      <div>{time}</div>
+      <div style={{ display: "flex" }}>
+        <p>{("0" + (Math.floor(time / 60000) % 60)).slice(-2)} : </p>
+        <p>{("0" + (Math.floor(time / 1000) % 60)).slice(-2)} : </p>
+        <p>{("0" + ((time / 10) % 100)).slice(-2)}</p>
+      </div>
     </div>
   );
 };
