dff7d33372eb0d8dcef8b345ca8b6a273725714c
Your Name <md1.zouaoui@gmail.com>
2022-05-24 00:57:26 +0200

    survey working

--- src/pages/stream/browserSource.js
+++ src/pages/stream/browserSource.js
@@ -25,21 +25,11 @@ const twitchDisconnect = async () => {
   tmiClient.disconnect();
 };
 
-let currentMessage = tmiClient.on(
-  "message",
-  (channel, tags, message, user, self) => {
-    return message;
-  }
-);
-
 const browserSource = ({ timersData, sondagesData }) => {
   const [countersData, setCountersData] = useState([]);
   const [surveysData, setSurveysData] = useState([]);
   const surveyRef = useRef();
-  const [percents, setPercents] = useState([]);
-  const [counter, setCounter] = useState(0);
   const [connected, setConnected] = useState(false);
-  const [username, setUsername] = useState("");
   const [test, setTest] = useState();
   const [isLoading, setIsLoading] = useState(true);
   const [fetching, setFetching] = useState(false);
@@ -50,25 +40,13 @@ const browserSource = ({ timersData, sondagesData }) => {
 
   useEffect(() => {
     surveyRef.current = {};
-    console.log("useEffect triggered");
-    console.log("tmi", tmiClient);
-
     const loadData = async () => {
       const counters = await client.query({ query: GET_COUNTERS });
       const survey = await client.query({ query: GET_SURVEYS });
-      console.log("survey", survey);
       setCountersData(counters.data.getCounters);
       setSurveysData(survey.data.getSurveys);
       surveyRef.current = survey.data.getSurveys[0];
-      setPercents(
-        survey.data.getSurveys[0].fields.map((item, index) => {
-          return 0;
-        })
-      );
-
-      console.log("data loaded");
       setIsLoading(false);
-
       if (survey.data.getSurveys[0].started) {
         setConnected(true);
         twitchConnect();
@@ -77,46 +55,19 @@ const browserSource = ({ timersData, sondagesData }) => {
         twitchDisconnect();
       }
     };
-
     loadData();
-
     const pusher = new Pusher("74661ff50b3ca8023bb7", {
       cluster: "eu",
       encrypted: true,
     });
     const channel = pusher.subscribe("browserSource");
     channel.bind("counterUpdated", (data) => {
-      // console.log("OH FONCTIONNE ZEUBI");
-      // console.log("received data", data);
       setTest("#" + Math.floor(Math.random() * 16777215).toString(16));
       setFetching(!fetching);
     });
     channel.bind("surveyUpdated", (data) => {
-      // console.log("OH FONCTIONNE ZEUBI");
-      // console.log("received data", data);
-
       setFetching(!fetching);
     });
-
-    tmiClient.on("message", (channel, tags, message, user, self) => {
-      setCounter((counter) => counter + 1);
-      console.log("yo", tags);
-      surveyRef.current.fields.forEach((field, index) => {
-        if (message == index + 1) {
-          setPercents((oldPercents) =>
-            oldPercents.map((current, currentIndex) => {
-              return currentIndex == index ? current + 1 : current;
-            })
-          );
-          setUsername(tags.username);
-        }
-      });
-    });
-    // const interval = setInterval(() => {
-
-    //   console.log("in set interval");
-    // }, 1000);
-    // return () => clearInterval(interval);
   }, [fetching]);
 
   return (
@@ -137,12 +88,7 @@ const browserSource = ({ timersData, sondagesData }) => {
           .filter((surveys) => surveys.isStreamed === true)
           .map((item, index) => (
             <div style={{ background: test, color: "white" }} key={index}>
-              <StreamedSurvey
-                surveyData={item}
-                percents={percents}
-                counter={counter}
-                currentUsername={username}
-              />
+              <StreamedSurvey surveyData={item} />
             </div>
           ))
       )}
@@ -150,42 +96,46 @@ const browserSource = ({ timersData, sondagesData }) => {
   );
 };
 
-const StreamedSurvey = ({ surveyData, percents, counter, currentUsername }) => {
+const StreamedSurvey = ({ surveyData }) => {
   const [index, setIndex] = useState(0);
   const [test, setTest] = useState(false);
   const [currentSurvey, setCurrentSurvey] = useState([]);
+  const [percents, setPercents] = useState([]);
+  const [username, setUsername] = useState("");
+  const [counter, setCounter] = useState(0);
+
   const testPos = useSpring({ x: 0, y: 0 });
   const bindPos = useDrag((params) => {
     testPos.x.set(params.offset[0]);
     testPos.y.set(params.offset[1]);
   });
 
-  let currentMessage = "Toz";
-
   useEffect(() => {
-    console.log("rerendered");
+    setPercents(
+      surveyData.fields.map((item, index) => {
+        return 0;
+      })
+    );
     if (surveyData.fields.length) {
-      console.log(surveyData.fields);
       surveyData.fields.forEach((item, index) => {
         setCurrentSurvey((oldSurvey) => [...oldSurvey, item]);
       });
+      tmiClient.on("message", (channel, tags, message, userstate, self) => {
+        console.log('userState' , userstate , tags)
+        surveyData.fields.forEach((item, index) => {
+          if (message == index + 1) {
+            setCounter((counter) => counter + 1);
+            setPercents((oldPercents) =>
+              oldPercents.map((current, currentIndex) => {
+                return currentIndex == index ? current + 1 : current;
+              })
+            );
+            setUsername(tags.username);
+          }
+        });
+      });
     }
   }, []);
-
-  // tmiClient.on("message", (channel, tags, message, self) => {
-  //   setCounter((counter) => counter + 1);
-
-  //   currentSurvey.forEach((field, index) => {
-  //     if (message == index + 1) {
-  //       let updatedCurrentSondage = currentSurvey.map((item, itemIndex) => {
-  //         return index == itemIndex
-  //           ? { ...item, percent: +item.percent + 1 }
-  //           : item;
-  //       });
-  //       setCurrentSurvey(updatedCurrentSondage);
-  //     }
-  //   });
-  // });
 
   return (
     <animated.div
@@ -214,7 +164,7 @@ const StreamedSurvey = ({ surveyData, percents, counter, currentUsername }) => {
           </div>
         ))}
       </div>
-      <div> {counter > 0 ? currentUsername : ""}</div>
+      <div> {counter > 0 ? username : ""}</div>
     </animated.div>
   );
 };
